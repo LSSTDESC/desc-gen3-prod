e88bcb96c7d354ac52385f39ba0b4252d73b5c80
David Adams <dladams@bnl.gov>
2023-06-20 07:56:09 -0700

    Development for 0.0.14. #1

--- scripts/g3wfpipe-run.py
+++ scripts/g3wfpipe-run.py
@@ -225,12 +225,9 @@ if showStatus:
     get_pg()
     futures = [job.get_future() for job in pg.values() if not job.dependencies]
     ntsk = len(futures)
-    statlogmsg(f"Workflow task count: {ntsk}")
     ndone = 0
-    while ndone < ntsk:
-        ndone = 0
-        for fut in futures:
-            if fut.done(): ndone += 1
-        statlogmsg(f"Finished {ndone} of {ntsk} tasks.")
+    for fut in futures:
+        if fut.done(): ndone += 1
+    statlogmsg(f"Finished {ndone} of {ntsk} tasks.")
 
 logmsg("All steps completed.")
